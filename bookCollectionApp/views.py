26a8d8cb3c45ecd51246ed99fc3df861d1c6b47b
kajami <>
2022-05-28 23:22:18 +0300

    Added LoginRequired mixin

--- bookCollectionApp/views.py
+++ bookCollectionApp/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = models.Book
+    raise_exception = True
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = models.Book
+    raise_exception = True
 
-class BookUpdateView(UpdateView):
+class BookUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Book
     fields = [
         'name',
@@ -22,12 +25,15 @@ class BookUpdateView(UpdateView):
         'addingDate',
     ]
     success_url ='/books/'
+    raise_exception = True
 
-class BookDeleteView(DeleteView):
+
+class BookDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Book
     success_url ='/books/'
+    raise_exception = True
 
-class BookCreateView(CreateView):
+class BookCreateView(LoginRequiredMixin, CreateView):
     model = models.Book
     template_name = 'bookCollectionApp/book_create.html'
     fields = [
@@ -39,6 +45,7 @@ class BookCreateView(CreateView):
         'addingDate',
     ]
     success_url ='/books/'
+    raise_exception = True
 
 #if user makes POST request on login.html, try authenticate. If user is found redirect to /books/.
 def login_user(request):
@@ -46,7 +53,7 @@ def login_user(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user:
             login(request, user)
             messages.success(request, ("Login successful."))
             return redirect('/books/')
@@ -61,7 +68,7 @@ def logout_user(request):
     messages.success(request, ("You were logged out."))
     return redirect('/')
 
-#if user makes POST request on register_user.html, program tries to create a new user with UserCreationForm, else it will only render UserCreationForm in page.
+#if user makes POST request on register_user.html, program creates a new user with UserCreationForm, else it will only render UserCreationForm in page.
 def register_user(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
