30583afd3f7aa8c532f478af5a05bf61d11ea075
kajami <>
2022-05-28 20:58:07 +0300

    first commit

--- myBookCollection/settings.py
+++ myBookCollection/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h4_ryj0p7&g9pp$ed_eol!g@g7@yu&5tcmazas1c4!vcatpwyk'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
